3e213affc27201492b429e4898eb2952cd2b5a81
TYC281 <eortega2@alumnes.aulabalear.org>
2023-11-16 18:40:50 +0000

    mi primer commit

---
+++ hola.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [$# = -eq 0]
+then
+	echo "ERROR"
+else
+	echo "Hola," "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8"
+fi
---
+++ hola_mundo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$#" -lt 1 ]; then
+	echo "ERROR; Escribe algo."
+	exit 1
+fi
+
+echo "Hola," "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+
